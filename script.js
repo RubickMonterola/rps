3e09cb11852f1ffde25e790ce05545bafcce9cd4
Rubick Monterola <rubickm10@gmail.com>
2022-09-22 15:15:56 +0800

    Added a getPlayerChoice function

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+//generate computer's chosen draw
 function getComputerChoice(){
     let computerChoice = null;
     let randomPick = Math.floor(Math.random() * 3) + 1;
     if (randomPick === 1){
-        computerChoice = 'Rock';
+        computerChoice = 'rock';
     } else if (randomPick === 2){
-        computerChoice = 'Paper';
+        computerChoice = 'paper';
     } else {
-        computerChoice = 'Scissors';
+        computerChoice = 'scissors';
     }
     return computerChoice;
 }
 
-getComputerChoice()
+//getPlayerChoice function
+function getPlayerChoice(promptedChoice){
+    let playerChoice = promptedChoice.toLowerCase();
+    switch(playerChoice){
+        case 'rock':
+        case 'paper':
+        case 'scissors':
+            return playerChoice;
+            break;
+        default:
+            return "an invalid choice";
+    }
+}
+
+//playRound function
+function playRound(){
+    
+}
+
+let promptInput = prompt("rock paper scissors shoot\nyour choice?: ");
+getPlayerChoice(promptInput);
+console.log("Player picked " + getPlayerChoice(promptInput));
+console.log("Computer picked " + getComputerChoice());
